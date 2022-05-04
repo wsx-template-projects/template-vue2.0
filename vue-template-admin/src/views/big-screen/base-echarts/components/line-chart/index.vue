7f9ed4e71e46af811efafe7ce8cd7ffc3a991a51
wangshengxian <m18696132633@163.com>
2022-05-04 23:52:31 +0800

    feat: 新增图表组件

---
+++ vue-template-admin/src/views/big-screen/base-echarts/components/line-chart/index.vue
@@ -0,0 +1,40 @@
+<!--
+   折线图 - 组件
+-->
+<template>
+    <div class="components-temp">
+        <h1>line-chart</h1>
+        <div :id="id" :style="{ width: width, height: height }"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentsTempLineChart',
+    components: {},
+    props: {
+        id: {
+            type: String,
+            default: 'chart',
+        },
+        width: {
+            type: String,
+            default: '200px',
+        },
+        height: {
+            type: String,
+            default: '200px',
+        },
+    },
+    data() {
+        return {}
+    },
+    computed: {},
+    created() {},
+    mounted() {},
+    methods: {},
+}
+</script>
+<style lang="scss" scoped>
+//@import url(); 引入公共css类
+</style>
